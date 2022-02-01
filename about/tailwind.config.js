fc18c946900147314b18535659ba8bba7f32eaf3
shinnthantminn <shinthantmin32@gmail.com>
2022-02-01 23:22:29 +0630

    Forabout

---
+++ about/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./public/**/*.css"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': ['Mochiy Pop P One'],
+        'card':["'Dancing Script', cursive"]
+      },
+      colors:{
+        left:"#70e1f5",
+        right:"#bdf33a"
+      }
+    },
+  },
+  plugins: [function({ addUtilities, addComponents, e, prefix, config }) {
+    const newUtilities = {
+      '.grid-row': {
+        gridRow: '1'
+      },
+      '.grid-row-none':{
+        gridRow: "auto"
+      },
+      ".wordSpacing": {
+        wordSpacing:"5px"
+    }
+    }
+    addUtilities(newUtilities)
+  }],
+}
